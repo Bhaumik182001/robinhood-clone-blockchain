cad8c2a49ac89b9d5ab87b26b05d5997b5e79c36
Bhaumik182001 <bhaumik182001@gmail.com>
2022-06-10 00:00:09 -0400

    Frontend mostly Complete

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,52 @@
 import { BiDotsHorizontalRounded } from 'react-icons/bi'
-
 import { AiOutlinePlus } from "react-icons/ai";
-
+import { Header } from '../components/Header';
+import PortfolioChart from '../components/PortfolioChart';
+import { BuyTokens } from '../components/BuyTokens';
+import { Notice } from '../components/Notice';
+import { Assests } from '../components/Assests';
 
 const styles = {
   wrapper: 'w-screen h-screen flex flex-col',
-  mainContainer: 'w-2/3 h-full m-auto flex mt-16',
-  leftMain: 'flex flex-col w-3/4 h-full  p-6 overflow-y-scroll',
-  portfolioAmountContainer: 'flex flex-col ',
+  mainContainer: `w-2/3 h-full m-auto flex mt-16`,
+  leftMain: 'flex flex-col w-3/4 h-fu;; p-6 overflow-y-scroll',
+  portfolioAmountContainer: 'flex flex-col',
   portfolioAmount: 'text-white text-4xl',
   portfolioPercent: 'text-white font-bold text-sm',
   pastHour: 'text-gray-400',
-  chartContainer:
-    'text-5xl flex justify-center w-full h-1/3 text-white mt-11 mb-11',
-  buyingPowerContainer:
-    'w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between items-center p-4',
-  buyingPowerTitle: 'text-white font-bolder text-lg',
-  buyingPowerAmount: 'text-white font-bolder text-xl',
+  chartContainer: 'text-5xl flex justify-center w-full h-1/3 text-white mt-11 mb-11',
+  buyingPowerContainer: 'w-full border-t mb-24 border-b h-16 border-[#30363b] flex justify-between item',
+  buyingPowerTitle: 'text-white font-border text-lg',
+  buyingPowerAmount: 'text-white font-border text-xl',
   notice: 'flex border border-[#30363b] mx-11 my-4 p-5 flex-col flex-1',
   noticeContainer: 'flex-1',
   noticeTitle: 'text-gray-500',
   noticeMessage: 'text-white font-bold',
-  noticeCTA: 'font-bold text-green-500 cursor-pointer mt-5',
-  rightMain:
-    'flex flex-col flex-1 h-4/5 bg-[#1E2123] mt-6 rounded-lg overflow-y-scroll noScroll',
+  noticeCTA: 'font-bold text-green-500 cursor pointer mt-5',
+  rightMain: 'flex flex-col flex-1 h-4/5 bg-[#1E2123] mt-6 rounded-lg overflow-y-scroll noScroll',
   rightMainItem: 'flex items-center text-white p-5 border-b border-[#30363b]',
-  ItemTitle: 'flex-1 font-bold',
+  ItemTitle: 'flex-1 flex-bold',
   moreOptions: 'cursor-pointer text-xl',
 }
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
+      <Header />
       <div className={styles.mainContainer}>
         <div className={styles.leftMain}>
 
         
         <div className={styles.portfolioAmountContainer}>
           <div className={styles.portfolioAmount}>23 ETH</div>
-          <div className={styles.portfolioPercent}>+0.0008(+0.57%)
+          <div className={styles.portfolioPercent}>
+            +0.0008(+0.57%)
               <span className={styles.pastHour}>Past Hour</span>
           </div>
         </div>
       <div>
         <div className={styles.chartContainer}>
-          {/* <PortfolioChart /> */}
+          <PortfolioChart />
         </div>
       </div>
       <div className={styles.buyingPowerContainer}>
@@ -56,17 +58,20 @@ export default function Home() {
           <div className={styles.noticeContainer}>
               <div className={styles.noticeTitle}>Send Funds</div>
               <div className={styles.noticeMessage}>Transfer Your Funds</div>
-              {/* <BuyTokens /> */}
+              <BuyTokens /> 
           </div>
       </div>
-      {/* <Notice /> */}
+        <Notice />
       </div>
       <div className={styles.rightMain}>
         <div className={styles.rightMainItem}>
           <div className={styles.ItemTitle}>Crypto Currencies</div>
          <BiDotsHorizontalRounded className={styles.moreOptions} />
-      
         </div>
+        <Assests  coin={"BTC"} price = {0.89}/>
+        <Assests  coin={"SOL"} price = {0.21}/>
+        <Assests  coin={"ETH"} price = {0.01}/>
+        <Assests  coin={"USDC"} price = {0.90}/>
         <div className={styles.rightMainItem}>
           <div className={styles.ItemTitle}>Lists</div>
           <AiOutlinePlus className={styles.moreOptions} />
